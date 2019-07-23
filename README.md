174afed09f44d17f70677bdf67c2b6351b83ba91
MORIMORI0317 <43478594+MORIMORI0317@users.noreply.github.com>
2019-07-24 00:16:53 +0900

    1.0.0.0

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
+#ItemDespawnCanseler
 ItemDespawnCanseler
 
 CuresForge
-https://www.curseforge.com/minecraft/mc-mods/itemdespawncanseler
+
 
 Discord
 https://discord.gg/X9BUF9A
--- src/main/java/com/morimori/itemdespawncanseler/ItemDespawnCanseler.java
+++ src/main/java/com/morimori/itemdespawncanseler/ItemDespawnCanseler.java
@@ -2,19 +2,33 @@ package com.morimori.itemdespawncanseler;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.item.ItemExpireEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(Variable.MODID)
+@Mod(modid = Variable.MODID, name = Variable.MODNAME, version = Variable.MODVER)
 public class ItemDespawnCanseler
 {
-    public ItemDespawnCanseler() {
-        IEventBus EVENT_BUS = MinecraftForge.EVENT_BUS;
-        EVENT_BUS.register(this);
-        ItemDespawnCanselerHandler ItemDespawnCanselerHandler = new ItemDespawnCanselerHandler();
-        EVENT_BUS.addListener(EventPriority.HIGH, false, ItemExpireEvent.class, ItemDespawnCanselerHandler::onItemExpire);
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
 
     }
 
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    	MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+    @SubscribeEvent
+	public void onItemExpire(ItemExpireEvent event) {
+		event.setCanceled(true);
+	}
 }
+
