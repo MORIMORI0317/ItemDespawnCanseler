7c38b16a9ab8e5cba03a480448bbd1d1664c8b92
MORIMORI0317 <43478594+MORIMORI0317@users.noreply.github.com>
2019-12-18 14:16:38 +0900

    1.0

--- src/main/java/com/morimori/itemdespawncanseler/ItemDespawnCanseler.java
+++ src/main/java/com/morimori/itemdespawncanseler/ItemDespawnCanseler.java
@@ -2,33 +2,19 @@ package com.morimori.itemdespawncanseler;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.item.ItemExpireEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = Variable.MODID, name = Variable.MODNAME, version = Variable.MODVER)
+@Mod(Variable.MODID)
 public class ItemDespawnCanseler
 {
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
+    public ItemDespawnCanseler() {
+        IEventBus EVENT_BUS = MinecraftForge.EVENT_BUS;
+        EVENT_BUS.register(this);
+        ItemDespawnCanselerHandler ItemDespawnCanselerHandler = new ItemDespawnCanselerHandler();
+        EVENT_BUS.addListener(EventPriority.HIGH, false, ItemExpireEvent.class, ItemDespawnCanselerHandler::onItemExpire);
 
     }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-    	MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-
-    }
-    @SubscribeEvent
-	public void onItemExpire(ItemExpireEvent event) {
-		event.setCanceled(true);
-	}
 }
-
